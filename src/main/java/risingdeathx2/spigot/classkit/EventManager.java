23aecffc240d588d62aed554efa228796d5ae954
Demoted <53052014+Bbro122@users.noreply.github.com>
2024-04-14 19:43:04 -0500

    fixed message relay

--- src/main/java/risingdeathx2/spigot/classkit/EventManager.java
+++ src/main/java/risingdeathx2/spigot/classkit/EventManager.java
@@ -1,21 +1,7 @@
 package risingdeathx2.spigot.classkit;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
-
-import net.luckperms.api.model.user.User;
-import net.md_5.bungee.api.ChatColor;
 
 /**
  * EventManager
