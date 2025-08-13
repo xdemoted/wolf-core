e62bc05801ce5c9d42de88eb3dcc4d4ee234ccd4
David Evans <53052014+xdemoted@users.noreply.github.com>
2025-08-13 00:38:42 -0500

    Nametag creation patch

--- main/src/main/java/com/wolfco/main/utility/FontUtil.java
+++ main/src/main/java/com/wolfco/main/utility/FontUtil.java
@@ -1,42 +1,11 @@
 package com.wolfco.main.utility;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.wolfco.main.Core;
 
 public class FontUtil {
-    static final Map<String, String> fontMap = new HashMap<>();
-    static {
-        fontMap.put("A", "ꑒ");
-        fontMap.put("B", "ꑓ");
-        fontMap.put("C", "ꑔ");
-        fontMap.put("D", "ꑕ");
-        fontMap.put("E", "ꑖ");
-        fontMap.put("F", "ꑗ");
-        fontMap.put("G", "ꑘ");
-        fontMap.put("H", "ꑙ");
-        fontMap.put("I", "ꑚ");
-        fontMap.put("J", "ꑛ");
-        fontMap.put("K", "ꑜ");
-        fontMap.put("L", "ꑝ");
-        fontMap.put("M", "ꑞ");
-        fontMap.put("N", "ꑟ");
-        fontMap.put("O", "ꑠ");
-        fontMap.put("P", "ꑡ");
-        fontMap.put("Q", "ꑢ");
-        fontMap.put("R", "ꑣ");
-        fontMap.put("S", "ꑤ");
-        fontMap.put("T", "ꑥ");
-        fontMap.put("U", "ꑦ");
-        fontMap.put("V", "ꑧ");
-        fontMap.put("W", "ꑨ");
-        fontMap.put("X", "ꑩ");
-        fontMap.put("Y", "ꑪ");
-        fontMap.put("Z", "ꑫ");
-    }
  
     static public String createNameTag(String name, String textColor) {
         StringBuilder sb = new StringBuilder();
@@ -44,20 +13,17 @@ public class FontUtil {
         String colorEnd = "</#" + textColor + ">";
         char[] chars = name.toCharArray();
 
-        sb.append("ꑬ");
+        sb.append("<font:block>¥");
 
-        for (int i = 0; i < chars.length; i++) {
-            if (i == chars.length - 1) sb.append("ꑭ");
-            else sb.append("ꑑ");
+        sb.append(colorStart).append(chars[0]).append(colorEnd);
 
+        for (int i = 1; i < chars.length; i++) {
+            sb.append("¤");
             String letter = String.valueOf(chars[i]);
-
-            if (fontMap.containsKey(letter.toUpperCase())) {
-                sb.append(colorStart).append(fontMap.get(letter.toUpperCase())).append(colorEnd);
-            } else {
-                sb.append(letter);
-            }
+            sb.append(colorStart).append(letter).append(colorEnd);
         }
+
+        sb.append("¢</font>");
 
         return sb.toString();
     }
